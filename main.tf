967cfa62a745160a764dd324113db8f2821a66a0
Ritesh Pandey <rp@prioritycloud.in>
2025-12-07 13:30:18 +0530

    Cleanup S3

--- main.tf
+++ main.tf
@@ -12,14 +12,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-resource "aws_s3_bucket" "my_lab_bucket" {
-  # Fix: Use lowercase, hyphens, and a random number
-  bucket = "terraform-lab-user-2025-x9z" 
-
-  tags = {
-    Name        = "My Lab Bucket"
-    Environment = "Dev"
-    Version     = "1.0"  # <--- NEW LINE ADDED
-  }
-}
